921d07241ac28fb310978b39f4f0439e8f216431
CuongLiam <114846183+CuongLiam@users.noreply.github.com>
2025-04-18 11:59:12 +0700

    Add files via upload

---
+++ javaProjects/Main/src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("Helloword! \n");
+        System.out.println("baba");
+    }
+
+}
---
+++ javaProjects/Main2/src/c1.java
@@ -0,0 +1,15 @@
+public class c1 {
+//    public static void main(String[] args) {
+//
+//        String str1 = "oh hey";
+//        String str2 = "no go away";
+//        String temp;
+//
+//        temp = str1;
+//        str1 = str2;
+//        str2 = temp;
+//
+//        System.out.println("str1: "+str1);
+//        System.out.println("str2: "+str2);
+//    }
+}
