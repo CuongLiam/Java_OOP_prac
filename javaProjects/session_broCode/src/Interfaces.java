a63b570efe1dbcb2195957c07aa196bcf83ac03d
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-06-12 06:34:25 +0700

    interfaces

---
+++ javaProjects/session_broCode/src/Interfaces.java
@@ -0,0 +1,41 @@
+public class Interfaces {
+    public static void inter_main(){
+
+        // interface - a template that can be applied to a class
+        // giong voi inheritance, nhung ma cu the hoa a class has/must do
+        // classes can apply more than 1 interface, inheritance chi co 1 super
+
+
+    }
+}
+
+class Rabbit implements Prey{
+    @Override
+    public void flee() {
+        System.out.println("the rabbit is fleeing");
+    }
+}
+class Snake implements Pred{
+    @Override
+    public void hunt() {
+        System.out.println("the snake is hunting");
+    }
+}
+class Fish implements Prey, Pred{
+    @Override
+    public void hunt() {
+        System.out.println("the fish is hunting");
+    }
+
+    @Override
+    public void flee() {
+        System.out.println("the fish is fleeing");
+    }
+}
+
+interface Prey{
+    void flee();
+}
+interface Pred{
+    void hunt();
+}
